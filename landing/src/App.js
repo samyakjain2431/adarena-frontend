91658199e42d2c92c4d30ef9de0bd5accda65d5a
samyakjain2431 <samyak.jn2431@gmail.com>
2025-04-24 13:34:36 +0530

    try

--- landing/src/App.js
+++ landing/src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          hello, everyone
         </p>
         <a
           className="App-link"
